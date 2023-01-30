fc76ce54b4704f5cd3e1fe9cd98326a186f206a7
JuliMelnikova <ulia682082@gmail.com>
2023-01-31 01:05:30 +0200

    Link general

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import Notiflix from 'notiflix';
 
 const form = document.querySelector(".form");
