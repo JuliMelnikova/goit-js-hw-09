c5bcfd498024f533fafb89e54175b04bd033500a
JuliMelnikova <ulia682082@gmail.com>
2023-01-31 00:56:02 +0200

    change import

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,6 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
@@ -29,7 +32,3 @@ btnStop.addEventListener('click', () => {
     btnStop.setAttribute('disabled', true);
     btnStart.removeAttribute('disabled', false);
 });
-
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
